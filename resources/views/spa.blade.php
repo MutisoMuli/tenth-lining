c713229dd4bd8649d8d97ffaaf7eb34aa1c7696f
Mutiso Muli <mulimutiso525@gmail.com>
2026-07-20 16:18:34 +0300

    phew

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -6,8 +6,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Tenth Lining – Professional Court Document Formatting</title>
     <meta name="description" content="Automatically format your legal documents with page numbering and tenth lining per Kenyan Court of Appeal Rules. Pay securely via M-Pesa.">
-    <link rel="icon" type="image/svg+xml" href="/tenth-lining/public/favicon.svg">
-    <link rel="alternate icon" href="/tenth-lining/public/favicon.ico">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+    <link rel="alternate icon" href="{{ asset('favicon.ico') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;600&family=Inter:wght@300;400;500;600;700;800;900&family=Lato:ital,wght@0,400;0,700;1,400&family=Merriweather:ital,wght@0,400;0,700;1,400&family=Montserrat:wght@400;600;700&family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400&family=Playfair+Display:ital,wght@0,400;0,700;1,400&family=Roboto:ital,wght@0,400;0,500;0,700;1,400&display=swap" rel="stylesheet">
@@ -18,11 +18,11 @@
     <div id="app"></div>
     <script>
         window.__APP_CONFIG__ = {
-            baseUrl: '/tenth-lining',
+            baseUrl: '{{ url('/') }}',
             csrfToken: '{{ csrf_token() }}',
         };
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.4.168/pdf.min.mjs" type="module"></script>
-    <script src="/tenth-lining/public/spa/app.js?v={{ time() }}" type="module"></script>
+    <script src="{{ asset('spa/app.js') }}?v={{ time() }}" type="module"></script>
 </body>
 </html>
